a503854de2546f58ddffb222eb06394e516391bb
bilykb <brettbilyk@gmail.com>
2021-07-24 19:26:37 +0000

    wrote a function that compares two arrays, and removes the elements in the second from the first

---
+++ without.js
@@ -0,0 +1,48 @@
+const without = function (arr, valuesToRemove) {
+  let newArr = [];
+
+  arr.forEach((element1) => {
+    valuesToRemove.forEach((element2) => {
+      newArr = arr.filter(element1 => element1 !== element2)
+    });
+  });
+  return newArr
+}
+
+// function without(arr, valuesToRemove) {
+//   const valuesToRemoveSet = new Set(valuesToRemove);
+//   let newArr = [];
+
+//   arr.forEach((element1) => {
+//     if (!valuesToRemoveSet.has(element1)) {  // not going to remove
+//       newArr.push(element1);
+//     }
+//   });
+//   console.log(newArr);
+// }
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`)
+      return false;
+      }
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return true
+}
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3])
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"])
