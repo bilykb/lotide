67562ef001b1f37ea13c8e39795f1ed0518248b2
bilykb <brettbilyk@gmail.com>
2021-07-25 17:38:41 +0000

    add flatten function for lotide

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const flatten = function(arr) {
+  let newArr = [];
+  if (!Array.isArray(arr)) {
+    return false;
+  }
+  arr.forEach((element) => {
+    newArr = newArr.concat(element);
+  });
+  return newArr;
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+};
+
+assertArraysEqual(flatter([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
