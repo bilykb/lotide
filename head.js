5890c9a5e5659bbd4c658676010e8ad8c1d7b049
bilykb <brettbilyk@gmail.com>
2021-07-25 21:21:30 +0000

    minor syntax changes

--- head.js
+++ head.js
@@ -1,12 +1,13 @@
 /**
  * head.js is a function that takes an array as an arguement, and returns the head of the array
- * @param {array} array 
+ * @param {array} array
  * @returns {array[0]}
  */
 
 const head = function(array) {
   return array[0];
-}
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
