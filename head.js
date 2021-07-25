cd5ab9154f36c415fd698f8e230c77e0161443f5
bilykb <brettbilyk@gmail.com>
2021-07-25 21:00:30 +0000

    add comments to function

--- head.js
+++ head.js
@@ -1,4 +1,12 @@
-// FUNCTION IMPLEMENTATION
+/**
+ * head.js is a function that takes an array as an arguement, and returns the head of the array
+ * @param {array} array 
+ * @returns {array[0]}
+ */
+
+const head = function(array) {
+  return array[0];
+}
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -7,11 +15,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(array) {
-  return array[0];
-}
 
 
-// TEST CODE
+
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
