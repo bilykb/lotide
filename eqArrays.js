07da8ac24c634c584246d4fd3a1e50daa00889fd
bilykb <brettbilyk@gmail.com>
2021-07-25 20:54:37 +0000

    rearrange function order

--- eqArrays.js
+++ eqArrays.js
@@ -5,14 +5,6 @@
  * @return {boolean}
  */
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
 
   if (arr1.length !== arr2.length) {
@@ -26,10 +18,13 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-
-
-
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
