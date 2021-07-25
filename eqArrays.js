ebec5b3c453695df93d0b0437f55b43fcb447f49
bilykb <brettbilyk@gmail.com>
2021-07-25 17:27:48 +0000

    add test cases

--- eqArrays.js
+++ eqArrays.js
@@ -29,3 +29,6 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3', '4']), false);
+
+
