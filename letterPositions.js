7c5b8ddff15e7ab264cce02478ccd2fe21499f89
bilykb <brettbilyk@gmail.com>
2021-07-26 23:13:35 +0000

    created countLetters.js.  Added framework for function, preliminary comments, and functions to test assertions

---
+++ letterPositions.js
@@ -0,0 +1,31 @@
+/**
+ * a function that accepts a string as an argument, and returns an object with an array of indexes for each letter
+ * @param {string} sentence a string of characters
+ */
+
+const letterPositions = function(sentence) {
+
+}
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1, arr2) {
+
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+};
