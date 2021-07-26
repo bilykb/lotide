214e5f69ee5c869090f76e48cdf13b518b3f90d7
bilykb <brettbilyk@gmail.com>
2021-07-26 22:41:00 +0000

    added an if statement to ignore " " characters

--- countLetters.js
+++ countLetters.js
@@ -6,12 +6,15 @@
 
 const countLetters = function(string) {
   const countingObject = {};
+
   for (const letter of string) {
+    if (letter !== " ") {
       if (!countingObject[letter]) {
       countingObject[letter] = 1;
       } else {
         countingObject[letter]++
     }
+  }
   }
   return countingObject
 }
@@ -24,4 +27,18 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-console.log(countLetters("Happy Hharbour"))
+const word = countLetters("Happy Harbour Comics")
+
+assertEqual(word["H"], 2);
+assertEqual(word["a"], 2);
+assertEqual(word["p"], 2);
+assertEqual(word["y"], 1);
+assertEqual(word[" "], undefined);
+assertEqual(word["r"], 2);
+assertEqual(word["b"], 1);
+assertEqual(word["o"], 2);
+assertEqual(word["u"], 1);
+assertEqual(word["C"], 1);
+assertEqual(word["m"], 1);
+assertEqual(word["i"], 1);
+assertEqual(word["s"], 1);
