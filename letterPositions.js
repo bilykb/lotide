148cacaccb0519f14608fdefb4d3644e84e9f8e9
bilykb <brettbilyk@gmail.com>
2021-07-27 01:22:29 +0000

    add test code, added some comments

--- letterPositions.js
+++ letterPositions.js
@@ -6,38 +6,51 @@
 const letterPositions = function(sentence) {
   const indexList = {};
   
-  for (const letter of sentence) {
+  for (let index = 0; index < sentence.length; index++) {
+    const letter = sentence[index];
+    // variable created to increase readability of code
+
     if (letter !== " ") {
       if (!indexList[letter]) {
-        indexList[letter] = Array.of(sentence.indexOf(letter))
+        indexList[letter] = [index];
       } else {
+        indexList[letter].push(index);
       }
     }
-  };
+  }
   return indexList;
-}
+};
 
-console.log(letterPositions("Hello World"))
+const eqArrays = function(arr1, arr2) {
 
-// const eqArrays = function(arr1, arr2) {
-
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let index = 0; index < arr1.length; index++) {
-//     if (arr1[index] !== arr2[index]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 
-// const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
 
-//   if (!eqArrays(arr1, arr2)) {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-//   } else {
-//     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-//   }
-// };
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+};
+
+const letter = letterPositions("hello world");
+
+assertArraysEqual(letter["h"], [0]);
+assertArraysEqual(letter["e"], [1]);
+assertArraysEqual(letter["l"], [2, 3, 9]);
+assertArraysEqual(letter["o"], [4, 7]);
+assertArraysEqual(letter["w"], [6]);
+assertArraysEqual(letter["r"], [8]);
+assertArraysEqual(letter["d"], [10]);
+assertArraysEqual(letterPositions("hello").e, [1]);
