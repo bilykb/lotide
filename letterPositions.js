3d81f0fcccaa00fc8f512decfc5e4ddecc39f02e
bilykb <brettbilyk@gmail.com>
2021-07-27 00:03:26 +0000

    working if statement which populates a new object

--- letterPositions.js
+++ letterPositions.js
@@ -4,28 +4,40 @@
  */
 
 const letterPositions = function(sentence) {
-
+  const indexList = {};
+  
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (!indexList[letter]) {
+        indexList[letter] = Array.of(sentence.indexOf(letter))
+      } else {
+      }
+    }
+  };
+  return indexList;
 }
 
-const eqArrays = function(arr1, arr2) {
+console.log(letterPositions("Hello World"))
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let index = 0; index < arr1.length; index++) {
-    if (arr1[index] !== arr2[index]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function(arr1, arr2) {
+
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let index = 0; index < arr1.length; index++) {
+//     if (arr1[index] !== arr2[index]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
 
-const assertArraysEqual = function(arr1, arr2) {
+// const assertArraysEqual = function(arr1, arr2) {
 
-  if (!eqArrays(arr1, arr2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  }
-};
+//   if (!eqArrays(arr1, arr2)) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+//   } else {
+//     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+//   }
+// };
