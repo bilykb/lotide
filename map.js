e3a533d842f646d5e842d3e8ae8c595dd6ff66e0
bilykb <brettbilyk@gmail.com>
2021-07-29 23:14:05 +0000

    various testing console.logs added to map function, as well as a for...of loop

--- map.js
+++ map.js
@@ -1,11 +1,13 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
-
-  console.log("array: ", array)
-  console.log("callback: ", callback)
-
   const results = [];
+
+  for (let item of array) {
+    console.log("item BEFORE: ", item);
+    console.log("item AFTER: ", callback(item));
+    console.log("---")
+  }
   return results;
 }
 
