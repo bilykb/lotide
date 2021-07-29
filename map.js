a2156d4c00e36751ca6a4eec404f56f28e2eedfc
bilykb <brettbilyk@gmail.com>
2021-07-29 23:10:08 +0000

    additional logic added to map function

--- map.js
+++ map.js
@@ -1,10 +1,13 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function() {
+const map = function(array, callback) {
+
+  console.log("array: ", array)
+  console.log("callback: ", callback)
+
   const results = [];
   return results;
 }
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
-
