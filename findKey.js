0aeda95ade4278c64ae4840b98cf451200a50636
bilykb <brettbilyk@gmail.com>
2021-07-30 16:36:11 +0000

    added test function and general comments

--- findKey.js
+++ findKey.js
@@ -1,7 +1,8 @@
 /**
- * 
- * @param {*} object 
- * @param {*} callback 
+ * findKey() accepts an object and callback function.  It iterates through the object and returns the first key for which the callback returns a truthy value. If not, returns undefined.
+ * @param {object} object object to inspect
+ * @param {function} callback function invoked per iteration
+ * @return {key} returns a key that is truthy
  */
 
 const findKey = function(object, callback) {
@@ -29,3 +30,12 @@ assertEqual(findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 0), undefined);
