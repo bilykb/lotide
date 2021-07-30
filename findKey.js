161dcd0d6dc6da11cfa7a06f56d1699589dbf87e
bilykb <brettbilyk@gmail.com>
2021-07-30 02:17:31 +0000

    findKey function declared, with assertEqual test function

---
+++ findKey.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {*} object 
+ * @param {*} callback 
+ */
+
+const findKey = function(object, callback) {
+  
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
