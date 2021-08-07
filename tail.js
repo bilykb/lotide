d5712e297ed6f91d3a1474eeb4513d74e36d392b
bilykb <brettbilyk@gmail.com>
2021-08-07 21:03:39 +0000

    test code in tail.js moved to tailTest.js.  Test code updated to more accurately test tail.js

--- tail.js
+++ tail.js
@@ -3,26 +3,4 @@ const tail = function(array) {
   return newArray;
 };
 
-const assertEqual = function(actual, expected) {
-  if (JSON.stringify(actual) === JSON.stringify(expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const example1 = tail([]);
-assertEqual(example1, []);
-
-const example2 = tail([1]);
-assertEqual(example2, []);
+module.exports = tail;
