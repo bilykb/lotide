a04e64888117ed61afb72f691a2fc0fd939569f2
bilykb <brettbilyk@gmail.com>
2021-08-07 21:48:18 +0000

    removed test code to be placed in assertArraysEqual.test.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -5,20 +5,7 @@
  * Console.log(Assertion pass or fail)
  */
 
-
-const eqArrays = function(arr1, arr2) {
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let index = 0; index < arr1.length; index++) {
-    if (arr1[index] !== arr2[index]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require("./eqArrays")
 
 const assertArraysEqual = function(arr1, arr2) {
 
@@ -28,6 +15,3 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   }
 };
-
-assertArraysEqual([5, 6, 7], [1, 2, 3]); // false
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
