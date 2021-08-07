0268a167df07d622bff42e7537d1f5dcf80bcb55
bilykb <brettbilyk@gmail.com>
2021-08-07 21:07:19 +0000

    comments added to tail.js

--- tail.js
+++ tail.js
@@ -1,3 +1,9 @@
+/**
+ * tail removes the first index of the array, and returns the "tail" of the array
+ * @param {array} array 
+ * @returns array
+ */
+
 const tail = function(array) {
   const newArray = array.slice(1);
   return newArray;
