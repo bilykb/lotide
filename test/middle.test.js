2ec7ab505ddeb9017eca88c8da0f06adfbfb9ea1
bilykb <brettbilyk@gmail.com>
2021-08-09 19:07:00 +0000

    test file using mocha and chai for middle.js.  Half of test code added.  More to come

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,9 +1,32 @@
-const assertArraysEqual = require("../assertArraysEqual")
-const middle = require("../middle")
+const assert = require('chai').assert;
+const middle = require("../middle");
 
-assertArraysEqual(middle([10]), []); // => []
-assertArraysEqual(middle([10, 20]), []); // => []
-assertArraysEqual(middle([10, 20, 30]), [20]); // => [20]
-assertArraysEqual(middle([10, 20, 30, 40, 50]), [30]); // => [30]
-assertArraysEqual(middle([10, 20, 30, 40]), [20, 30]); // => [20, 30]
-assertArraysEqual(middle([10, 20, 30, 40, 50, 60]), [30, 40]); // [30, 40]
+describe("#middle", () => {
+  it('should return an empty array if an array has >= 2 length', () =>{
+    const array = [];
+
+    assert.deepEqual(middle(array), [])
+  })
+  it('should return an empty array if the array has a length of 2', () => {
+    const array = [10, 20];
+  
+    assert.deepEqual(middle(array), []);
+  });
+  it('should return [20] for the array [10, 20, 30]', () => {
+    const array = [10, 20, 30];
+
+    assert.deepEqual(middle(array), [20]);
+  });
+  it('should return [30] for the array [10, 20, 30, 40, 50]', () => {
+    const array = [10, 20, 30, 40, 50];
+
+    assert.deepEqual(middle(array), [30])
+  })
+});
+
+
+
+// assertArraysEqual(middle([10, 20, 30]), [20]); // => [20]
+// assertArraysEqual(middle([10, 20, 30, 40, 50]), [30]); // => [30]
+// assertArraysEqual(middle([10, 20, 30, 40]), [20, 30]); // => [20, 30]
+// assertArraysEqual(middle([10, 20, 30, 40, 50, 60]), [30, 40]); // [30, 40]
