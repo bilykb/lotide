147d56fba1c3eb75a817c1a17433310c2b95ade0
bilykb <brettbilyk@gmail.com>
2021-08-10 01:30:04 +0000

    ran through eslint

--- test/findKeyByValue.test.js
+++ test/findKeyByValue.test.js
@@ -3,28 +3,28 @@ const findKeyByValue = require("../findKeyByValue");
 
 describe("#findKeyByValue", () => {
   it("should return undefined when provided a value that is not in the object", () => {
-    const bestTVShowsByGenre = { 
-      sci_fi: "The Expanse",
+    const bestTVShowsByGenre = {
+      sciFi: "The Expanse",
       comedy: "Brooklyn Nine-Nine",
       drama:  "The Wire"
     };
-    assert.equal(findKeyByValue(bestTVShowsByGenre, "The Simpsons"), undefined)
+    assert.equal(findKeyByValue(bestTVShowsByGenre, "The Simpsons"), undefined);
   });
   it("should return 'comedy' when provided the value 'Brooklyn Nine-Nine'", () => {
-    const bestTVShowsByGenre = { 
-      sci_fi: "The Expanse",
+    const bestTVShowsByGenre = {
+      sciFi: "The Expanse",
       comedy: "Brooklyn Nine-Nine",
       drama:  "The Wire"
-    }
+    };
     assert.equal(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
   });
   it("should return 'undefined' if provided an empty string", () => {
-    const bestTVShowsByGenre = { 
-      sci_fi: "The Expanse",
+    const bestTVShowsByGenre = {
+      sciFi: "The Expanse",
       comedy: "Brooklyn Nine-Nine",
       drama:  "The Wire"
-    }
-    assert.isUndefined(findKeyByValue(bestTVShowsByGenre, " "), undefined)
+    };
+    assert.isUndefined(findKeyByValue(bestTVShowsByGenre, " "), undefined);
   });
   it("should return 'drama' if provided the string 'the wire'", () => {
     const bestTVShowsByGenre = {
