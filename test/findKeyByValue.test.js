692dcae840ad6f55ec0369f9b1dff560bbcb66a5
bilykb <brettbilyk@gmail.com>
2021-08-10 01:24:30 +0000

    all test code moved from findKeyByValue.js to findKeyByValue.test.js.  Additional test cases added

---
+++ test/findKeyByValue.test.js
@@ -0,0 +1,37 @@
+const assert = require("chai").assert;
+const findKeyByValue = require("../findKeyByValue");
+
+describe("#findKeyByValue", () => {
+  it("should return undefined when provided a value that is not in the object", () => {
+    const bestTVShowsByGenre = { 
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    };
+    assert.equal(findKeyByValue(bestTVShowsByGenre, "The Simpsons"), undefined)
+  });
+  it("should return 'comedy' when provided the value 'Brooklyn Nine-Nine'", () => {
+    const bestTVShowsByGenre = { 
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    }
+    assert.equal(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  });
+  it("should return 'undefined' if provided an empty string", () => {
+    const bestTVShowsByGenre = { 
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    }
+    assert.isUndefined(findKeyByValue(bestTVShowsByGenre, " "), undefined)
+  });
+  it("should return 'drama' if provided the string 'the wire'", () => {
+    const bestTVShowsByGenre = {
+      sciFi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    };
+    assert.equal(findKeyByValue(bestTVShowsByGenre, "the wire"), "drama");
+  });
+});
