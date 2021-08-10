180e245893446615c39bb046d30e933973d79ebf
bilykb <brettbilyk@gmail.com>
2021-08-10 23:40:14 +0000

    additional test code added to countOnly.test.js.  Code added to countOnly.js to accomodate test code

--- countOnly.js
+++ countOnly.js
@@ -2,10 +2,18 @@
  * a function which tallies how many times specific elements occur within an array
  * @param {array} allItems an array of strings that we need to look through
  * @param {object} itemstoCount an object specifying what to count
+ * @returns {object} 
  */
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
+
+  if (!Array.isArray(allItems)) {
+    throw new Error("Error Will Robinson!")
+  }
+  if (typeof itemsToCount !== "object" || typeof itemsToCount === null) {
+    throw new Error("Error Will Robinson!");
+  }
 
   for (const item of allItems) {
     if (itemsToCount[item]) {
--- test/countOnly.test.js
+++ test/countOnly.test.js
@@ -23,7 +23,7 @@ describe("#countOnly", () => {
 
     assert.equal(results['Jason'], 1);
   });
-  it("when given the name 'Karima,' a name that you are not looking for, it should return undefined", () => {
+  it("when given the name 'Agouhanna,' a name that you are not looking for, it should return undefined", () => {
     const firstNames = [
       "Karl",
       "Salima",
@@ -41,7 +41,7 @@ describe("#countOnly", () => {
       "Fang": true,
       "Agouhanna": false
     });
-    assert.isUndefined(results['Karima'], undefined);
+    assert.isUndefined(results['Agouhanna'], undefined);
   });
   it("when given the name 'Brett', a name that you are looking for but not part of the array you are searching, it returns undefined", () => {
     const firstNames = [
@@ -82,5 +82,48 @@ describe("#countOnly", () => {
       "Agouhanna": false
     });
     assert.equal(results['Fang'], 2);
+  });
+  it('it will throw error if the first argument that is not an array', () => {
+
+    assert.throw(() => {
+      countOnly(5, {
+        "Jason": true,
+        "Karima": true,
+        "Fang": true,
+        "Agouhanna": false
+      });
+    }, Error);
+  });
+  it('it will throw error if the second argument is not an object', () => {
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    assert.throw(() => {
+      countOnly(firstNames, "hello");
+    });
+  });
+  it('it will throw error if the second argument is null', () => {
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    assert.throw(() => {
+      countOnly(firstNames, null);
+    });
   });
 });
