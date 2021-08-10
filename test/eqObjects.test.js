ef44e459615db016bc8a2f855fcfb2d1a3289fc0
bilykb <brettbilyk@gmail.com>
2021-08-10 15:20:27 +0000

    test code updated to utilize chai and test eqObjects.js

--- test/eqObjects.test.js
+++ test/eqObjects.test.js
@@ -1,28 +1,41 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
-const ab = { a : "1", b : "2" };
-const ba = { b : "2", a : "1" };
-const xy = { x : "3", y : "4" };
-const yx = { y : "5", x : "1" };
-const zx = { z : "3", x : "4" };
+describe("#eqObjects", () => {
+  it('should return true when comparing { a : "1", b : "2" } to { a : "1", b : "2" }', () => {
 
-assertEqual(eqObjects(ab, ba), true);
-assertEqual(eqObjects(xy, yx), false);
-assertEqual(eqObjects(xy, zx), false);
+    assert.deepEqual(eqObjects({ a : "1", b : "2" }, { a : "1", b : "2" }), true);
+  });
+  it('should return false when comparing { x : "3", y : "4" } and { y : "5", x : "1" }', () => {
+   
+    assert.deepEqual(eqObjects({ x : "3", y : "4" }, { y : "5", x : "1" }), false);
+  });
+  it('should return false when comparing { x : "3", y : "4" } and { z : "3", x : "4" }', () => {
 
-const abc = { a : "1", b : "2", c : "3" };
-const cd = { c : "1", d : ["2", 3] };
-const dc = { d : ["2", 3], c : "1" };
-const cd2 = { c : "1", d : ["2", 3, 4] };
-const dc2 = { d : ["2", 3, 4], c : "1"  };
-const ae = { a : "1", e : ["2", 3, 4] };
-const emp1 = {};
-const emp2 = {};
+    assert.deepEqual(eqObjects({ x : "3", y : "4" }, { z : "3", x : "4" }), false);
+  });
+  it('should return true when comparing { c : "1", d : ["2", 3] } and { d : ["2", 3], c : "1" }', () => {
 
-assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqObjects(ab, abc), false);
-assertEqual(eqObjects(cd, cd2), false);
-assertEqual(eqObjects(ae, cd2), false);
-assertEqual(eqObjects(cd2, dc2), true);
-assertEqual(eqObjects(emp1, emp2), true);
+    assert.deepEqual(eqObjects({ c : "1", d : ["2", 3] }, { d : ["2", 3], c : "1" }), true);
+  });
+  it('should return false when comparing { a : "1", b : "2" } and { a : "1", b : "2", c : "3" }', () => {
+    
+    assert.deepEqual(eqObjects({ a : "1", b : "2" }, { a : "1", b : "2", c : "3" }), false);
+  });
+  it('should return false when comparing { c : "1", d : ["2", 3] } and { c : "1", d : ["2", 3, 4] }', () => {
+
+    assert.deepEqual(eqObjects({ c : "1", d : ["2", 3] }, { c : "1", d : ["2", 3, 4] }), false);
+  });
+  it('should return false when comparing { a : "1", e : ["2", 3, 4] } and { c : "1", d : ["2", 3, 4] }', () => {
+
+    assert.deepEqual(eqObjects({ a : "1", e : ["2", 3, 4] }, { c : "1", d : ["2", 3, 4] }), false);
+  });
+  it('should return true when comparing { c : "1", d : ["2", 3, 4] } and { d : ["2", 3, 4], c : "1"  }', () => {
+
+    assert.deepEqual(eqObjects({ c : "1", d : ["2", 3, 4] }, { d : ["2", 3, 4], c : "1"  }), true);
+  });
+  it('should return true when comparing {} and {}', () => {
+
+    assert.deepEqual(eqObjects({}, {}), true);
+  });
+});
