9c610330c3c542dd08474688ef705e0e45ed0a11
bilykb <brettbilyk@gmail.com>
2021-08-11 01:58:37 +0000

    removed two functions, not needed

--- without.js
+++ without.js
@@ -24,30 +24,4 @@ const without = function(arr, itemsToRemove) {
   return newArr;
 };
 
-const eqArrays = function(arr1, arr2) {
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let index = 0; index < arr1.length; index++) {
-    if (arr1[index] !== arr2[index]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-
-  if (!eqArrays(arr1, arr2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
-  }
-  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  return true;
-};
-
-assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]);
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
-
 module.exports = without;
