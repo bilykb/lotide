2a11bf6366e2012356c6694d022d43c1187308c3
bilykb <brettbilyk@gmail.com>
2021-08-11 01:20:57 +0000

    changes to comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,5 @@
 /**
-  * a function an searches for a key on an object where its value matches a given value
+  * a function that searches for a key in an object
  * @param {object} objectOfGenres an object with a list of best shows by genre
  * @param {string} tvShow a string of a television show name
  * @return {keyof Object}
